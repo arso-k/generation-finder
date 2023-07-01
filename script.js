6e1b71d2e1f89ac3c5aacc814131beececf79fc8
Arsonela <87339151+arso-k@users.noreply.github.com>
2023-07-01 21:30:02 +0200

    update

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 function identifyGeneration() {
-  var birthYear = document.getElementById("birthYear").value;
-  var generation = "";
+  var birthYear = document.getElementById('birthYear').value;
+  var generation = '';
 
-  if (birthYear >= 1901 && birthYear <= 1924) {
-    generation = "The Greatest Generation";
-  } else if (birthYear >= 1925 && birthYear <= 1945) {
-    generation = "The Silent Generation";
-  } else if (birthYear >= 1946 && birthYear <= 1964) {
-    generation = "The Baby Boomer Generation";
-  } else if (birthYear >= 1965 && birthYear <= 1979) {
-    generation = "Generation X";
-  } else if (birthYear >= 1980 && birthYear <= 1994) {
-    generation = "Millennials";
-  } else if (birthYear >= 1995 && birthYear <= 2012) {
-    generation = "Generation Z";
-  } else if (birthYear >= 2013 && birthYear <= 2025) {
-    generation = "Gen Alpha";
-  } else {
-    generation = "Unknown";
+  switch (true) {
+    case birthYear >= 1901 && birthYear <= 1924:
+      generation = 'The Greatest Generation';
+      break;
+    case birthYear >= 1925 && birthYear <= 1945:
+      generation = 'The Silent Generation';
+      break;
+    case birthYear >= 1946 && birthYear <= 1964:
+      generation = 'The Baby Boomer Generation';
+      break;
+    case birthYear >= 1965 && birthYear <= 1979:
+      generation = 'Generation X';
+      break;
+    case birthYear >= 1980 && birthYear <= 1994:
+      generation = 'Millennials';
+      break;
+    case birthYear >= 1995 && birthYear <= 2012:
+      generation = 'Generation Z';
+      break;
+    case birthYear >= 2013 && birthYear <= 2025:
+      generation = 'Gen Alpha';
+      break;
+    default:
+      generation = 'Unknown';
   }
 
-  document.getElementById("result").innerText = "You belong to: " + generation;
+  document.getElementById('result').innerText = 'You belong to: ' + generation;
 }
